670118038d6096c76be403c6fe5a3f6e32c0020d
lichen.hys <lichen@mail.haoyisheng.com>
2016-10-13 14:34:37 +0800

    V1.0_fixup修改

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,10 +1,15 @@
 package com;
 
 public class Test {
-
+    private String name;
     public static void main(String[] args) {               
         System.out.println("de678bc");
         System.out.println("test");
     }
-
+    public String getName() {
+        return name+"abc";
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }

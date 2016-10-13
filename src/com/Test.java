c55d02577288ea27187484d54d2715fc034b85a9
lichen.hys <lichen@mail.haoyisheng.com>
2016-10-13 11:07:10 +0800

    解决冲突

--- src/com/Test.java
+++ src/com/Test.java
@@ -2,8 +2,8 @@ package com;
 
 public class Test {
 
-    public static void main(String[] args) {       
-        System.out.println("1deftest2");
+    public static void main(String[] args) {               
+        System.out.println("de678bc");
     }
 
 }
